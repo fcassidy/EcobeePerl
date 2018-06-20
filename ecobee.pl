3a59447dd6d494c227ba7f863cd6dc9fb68a18e1
Frank Cassidy <fcassidy@iMac-5k.local>
2018-06-19 20:47:32 -0600

    first draft of query calls

--- ecobee.pl
+++ ecobee.pl
@@ -7,28 +7,39 @@ use strict;
 use warnings;
 
 
-#use JSON        qw(from_json);
+my $api_key;
+my $refresh_token;
+my $access_token;
 
+#read access and refresh token from file
+my $filename = 'tokens.dat';
+open(my $fh, '+<', $filename)
+  or die "Could not open file '$filename' $!";
 
+while (my $row = <$fh>) {
+  if($row =~ m/api_key:(\S*)/)
+  {
+    $api_key = $1;
+  }
 
-#    "access_token": "aytQ0McY9p7FjJVacHXyK0DtoKcNxwDr",
-#    "token_type": "Bearer",
-#    "expires_in": 3599,
-#    "refresh_token": "7JMivVLPz6DCsv6iZojt9rIxy8dHSX9p",
-#    "scope": "smartWrite"
+  if($row =~ m/refresh_token:(\S*)/)
+  {
+    $refresh_token = $1;
+  }
 
-my $refresh_token = "74rSWLiqMIYokRhogArj8vueGZoadBLa";
-my $api_key = "ffmUfLJkCfyNg6sN2lAyj4ODPZokpubL";
+  if($row =~ m/access_token:(\S*)/)
+  {
+    $access_token = $1;
+  }
 
+  print "$row\n";
+}
+
+print "access = $api_key, refresh = $refresh_token\n";
+
+#refresh tokens
 my $ua = LWP::UserAgent->new();
-
 my $url  = "https://api.ecobee.com/token";
-#my $data = "?grant_type=refresh_token&code=".$refresh_token."&client_id=".$api_key;
-
-#my $req = HTTP::Request->new(POST => $url);
-#my $response = $ua->post($url, $data);
-#my $response = $ua->request($req);
-
 my $response = $ua->post($url,
   ['grant_type' => 'refresh_token',
    'code' => $refresh_token,
@@ -36,52 +47,28 @@ my $response = $ua->post($url,
 
 print $response->decoded_content();
 
-$response->decoded_content() =~ m/(?s)"access_token":\s*\"(\S*)\",.*?"refresh_token":\s*(\S*),/;
+$response->decoded_content() =~ m/(?s)"access_token":\s*\"(\S*)\",.*?"refresh_token":\s*\"(\S*)\",/;
 my $new_access_token = $1;
 my $new_refresh_token = $2;
 
 print "access = $new_access_token, refresh = $new_refresh_token\n";
 
-#my $uri = 'https://api.ecobee.com/1/thermostat';
-my $uri = 'https://api.ecobee.com/1/thermostat?format=json&body=';
-my $req = HTTP::Request->new( 'GET', $uri );
-$req->header('Content-Type' => 'text/json');
-$req->header('Authorization' => 'Bearer '.$new_access_token);
+#store updated tokens to file
+seek $fh, 0, 0;
+print $fh "api_key:$api_key\n";
+print $fh "refresh_token:$new_refresh_token\n";
+print $fh "access_token:$new_access_token\n";
+close($fh);
 
-#*  SSL certificate verify ok.
-#> GET /1/thermostat?format=json&body={"selection":{"selectionType":"registered","selectionMatch":"","includeRuntime":true,"includeAlerts":true}} HTTP/1.1
-#> Host: api.ecobee.com
-#> User-Agent: curl/7.54.0
-#> Accept: */*
-#> Content-Type: text/json
-#> Authorization: Bearer kOKTDWKnlv13IZ7SfhxYuxo9M6oy3Dn4
-#>
-#< HTTP/1.1 200 OK
-#< Date: Tue, 19 Jun 2018 22:1
-
-#my $json = "format=json&body=";
-#$json .= "{\"selection\":{\"selectionType\":\"registered\",\"selectionMatch\":\"\",\"includeRuntime\":true}}";
-#$req->content($json);
-#my $res = $ua->request($req);
-
-my $json = "{\"selection\":{\"selectionType\":\"registered\",\"selectionMatch\":\"\",\"includeRuntime\":true}}";
+#make the request for thermostat information
+my $json = "{\"selection\":{\"selectionType\":\"registered\",\"selectionMatch\":\"\",\"includeRuntime\":true,\"includeSensors\":true}}";
+my $uri = 'https://api.ecobee.com/1/thermostat?format=json&body='.$json;
 
 my $res = $ua->get($uri,
   'Content-Type' => 'text/json',
   'Authorization' => 'Bearer '.$new_access_token,
-  'content' => $json
 );
-#my $response = $ua->get($denver_weather_url);
 
-
-#print $req->url;
-#print "\n";
-#print $req->as_string;
-#print "\n";
-#print $req->decoded_content();
-
-print "\n\n---------------------------------\n\n";
-print $res->content;
 print "\n\n---------------------------------\n\n";
 print $res->decoded_content();
 
